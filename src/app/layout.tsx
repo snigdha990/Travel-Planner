55fa5424eb9b2b740181523b813fc90af728ba94
snigdha990 <saisnigdha1811>
2025-09-17 17:05:21 +0530

    second commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import { ClerkProvider } from '@clerk/nextjs';
 import Header from "@/components/header";
 import Footer from "@/components/footer";
-
-import {
-  ClerkProvider,
-} from '@clerk/nextjs'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
